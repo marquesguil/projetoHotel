c5b9a9e7101921c577ed1f7941018377c1fad49d
guilherme.de.marques <guilherme.de.marques@accenture.com>
2020-05-10 19:58:06 -0300

    criação de classes de dominio

--- src/main/java/br/com/hotel/ProjectHotelApplication.java
+++ src/main/java/br/com/hotel/ProjectHotelApplication.java
@@ -1,5 +1,10 @@
 package br.com.hotel;
 
+/**
+ * @author guilherme.de.marques
+ *
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/br/com/hotel/dominio/funcionario.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.hotel.dominio;
+
+/**
+ * @author guilherme.de.marques
+ *
+ */
+public class funcionario extends pessoa {
+
+	/**
+	 * 
+	 */
+	public funcionario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
