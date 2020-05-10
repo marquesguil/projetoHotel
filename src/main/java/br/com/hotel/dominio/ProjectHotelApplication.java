dbd5ae89614bbb10db4ea722cf2fb8cb3120532f
guilherme.de.marques <guilherme.de.marques@accenture.com>
2020-05-09 21:02:21 -0300

    migrado para o spring

---
+++ src/main/java/br/com/hotel/dominio/ProjectHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotel.dominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hotel/dominio/ProjectHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hotel.dominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
